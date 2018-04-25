cb090dd80f85d5d567ac850b1924b8076fa38d4e
Logan Boydell <38225835+LBo98684@users.noreply.github.com>
2018-04-24 22:52:05 -0700

    Update IronScripter-Prelude-1.ps1
    Added a check to confirm existence of at least 1 capital letter. If not present a randomly chosen letter is capitalized. General code cleanup and commenting

--- IronScripter-Prelude-1.ps1
+++ IronScripter-Prelude-1.ps1
@@ -1,4 +1,4 @@
-﻿function New-Password
+function New-Password
 {
   [CmdletBinding()]
   param(
@@ -8,33 +8,53 @@
 
     Begin
     {
+      # Warn if nonalpha greater then length
       if($NonAlpha -gt $Length)
         {Write-Warning -Message "NonAlpha characters are greater than the total length";break}
-      $pswd = @()
+      
+      # Blank array list for creating password string
+      [System.Collections.ArrayList]$pswd = @()
+
+      # Blank array list for checking at least 1 capital letter
+      [System.Collections.ArrayList]$capLetters = @()
+
+      # Character sets used to build password
       $nonAplhaColl = ([char[]]([char]33..[char]64) + ([char[]]([char]91..[char]96)))
       $alphaColl = ([char[]]([char]65..[char]90)) + ([char[]]([char]97..[char]122))
     }
     Process
     {
-      $x = 0
+      # Add nonAlpha chars
+      $count = 0
       do
       {
-        $pswd += (Get-Random -InputObject $nonAplhaColl)    
-        $x++
+        $pswd.Add($(Get-Random -InputObject $nonAplhaColl)) *>$null
+        $count++
       }
-      until ($x -eq $NonAlpha)
+      until($count -eq $NonAlpha)
       
-      $x = 0
+      # Add alpha chars
+      $count = 0
       do
       {
-        $pswd += (Get-Random -InputObject $alphaColl)    
-        $x++
+        $pswd.Add($(Get-Random -InputObject $alphaColl)) *>$null
+        $count++
       }
-      until ($x -eq ($length - $NonAlpha))      
+      until ($count -eq ($length - $NonAlpha))
+      
+      # Confirm at least 1 capital letter
+      $pswd | foreach {if([char]::IsUpper($_)){$capLetters.Add($_) *> $null}}
+      if($capLetters.Count -eq 0)
+        {
+          # If no capital letters, capitalize first letter found in $pswd
+          $firstLetter = $($pswd -cmatch '[a-z]')[0].tostring()
+          $pswd = $pswd -replace $firstLetter,$firstLetter.toUpper() 
+        }
 
     }
     End
     {
-      ($pswd | sort {Get-Random}) -join ''
+      # Randomize $pswd and output array into a single string
+      ($pswd | Sort-Object {Get-Random}) -join ''
     }
 }
